3f77e4f8a8654e537d46c1a42e446975427e94fc
malexj <alexmaximovj@gmail.com>
2018-11-08 20:33:41 +0200

    add the implementation ListItterator

--- src/main/java/chapter_05/base/iterator/impl/LinkListImpl.java
+++ src/main/java/chapter_05/base/iterator/impl/LinkListImpl.java
@@ -3,38 +3,38 @@ package chapter_05.base.iterator.impl;
 import chapter_05.base.iterator.LinkList;
 import chapter_05.base.iterator.ListIterator;
 import chapter_05.base.iterator.entity.Link;
+import java.util.Objects;
 
-public class LinkListImpl implements LinkList
-{
+public class LinkListImpl implements LinkList {
+
     private Link link;
 
     @Override
-    public Link getFirs()
-    {
-        return null;
+    public Link getFirs() {
+        return this.link;
     }
 
     @Override
-    public void setFirst(Link link)
-    {
-
+    public void setFirst(Link link) {
+        this.link = link;
     }
 
     @Override
-    public boolean isEmpty()
-    {
-        return false;
+    public boolean isEmpty() {
+        return Objects.isNull(this.link);
     }
 
     @Override
-    public ListIterator getListIterator()
-    {
-        return null;
+    public ListIterator getListIterator() {
+        return new ListIteratorImpl(this);
     }
 
     @Override
-    public void displayList()
-    {
-
+    public void displayList() {
+        Link current = this.link;
+        while (!Objects.isNull(current)) {
+            current.displayLink();
+            current = current.getNext();
+        }
     }
 }
--- src/main/java/chapter_05/base/iterator/impl/ListIteratorImpl.java
+++ src/main/java/chapter_05/base/iterator/impl/ListIteratorImpl.java
@@ -3,9 +3,10 @@ package chapter_05.base.iterator.impl;
 import chapter_05.base.iterator.LinkList;
 import chapter_05.base.iterator.ListIterator;
 import chapter_05.base.iterator.entity.Link;
+import java.util.Objects;
 
-public class ListIteratorImpl implements ListIterator
-{
+public class ListIteratorImpl implements ListIterator {
+
     /**
      * the current link
      */
@@ -19,7 +20,58 @@ public class ListIteratorImpl implements ListIterator
     /**
      * the linked list
      */
-    private LinkList ourList;
+    private LinkList list;
 
+    public ListIteratorImpl(LinkListImpl list) {
+        this.list = list;
+        reset();
+    }
 
+    /**
+     * Start at first
+     */
+    @Override
+    public void reset() {
+        this.current = this.list.getFirs();
+        this.previous = null;
+    }
+
+    /**
+     * Check end of list
+     *
+     * @return true if last link
+     */
+    @Override
+    public boolean atEnd() {
+        return Objects.isNull(this.current.getNext());
+    }
+
+    /**
+     * go to next link
+     */
+    @Override
+    public void nextLink() {
+        this.previous = this.current;
+        this.current = this.current.getNext();
+    }
+
+    @Override
+    public Link getCurrent() {
+        return this.current;
+    }
+
+    @Override
+    public void insertAfter(int data) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void insertBefore(int data) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public long deleteCurrent() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
